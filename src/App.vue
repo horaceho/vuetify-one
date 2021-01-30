5a17c604c3f2be088c001f351a08c4a7e3d8c6ba
Horace Ho <horaceh@gmail.com>
2021-01-30 22:16:13 +0800

    update logo

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('./assets/logo.svg')"
           transition="scale-transition"
           width="40"
         />
@@ -20,7 +20,7 @@
           class="shrink mt-1 hidden-sm-and-down"
           contain
           min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          :src="require('./assets/logo.svg')"
           width="100"
         />
       </div>
